b8bbd4ce1ebb0babad3fe6ee59a8f25ad4d2786a
Adam Paulík <pauliada@fel.cvut.cz>
2017-05-17 00:42:17 +0200

    Modify App

--- src/main/java/cz/cvut/fel/pauliada/iot/runtime/App.java
+++ src/main/java/cz/cvut/fel/pauliada/iot/runtime/App.java
@@ -6,12 +6,10 @@ import org.apache.camel.impl.DefaultCamelContext;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        if (new RaspberryPiBoardFactory().isCompatibleWithPlatform()) {
             CamelContext context = new DefaultCamelContext();
             context.addRoutes(new RaspberryPiBh1750RouteBuilder());
             context.addRoutes(new RaspberryPiGpioRouteBuilder());
             context.start();
             Thread.sleep(Long.MAX_VALUE);
-        }
     }
 }
